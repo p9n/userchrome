edbbb10fb639322381ba59ac2f1ff76c72f03b54
Aoyama Nanami <aoyama_nanami@sakurasou>
2021-01-07 00:03:09 +0800

    add initial user.js

---
+++ user.js
@@ -0,0 +1,22 @@
+/* open everything in new tab */
+user_pref("browser.tabs.loadBookmarksInTabs", true);
+user_pref("browser.urlbar.openintab", true);
+
+/* disable url bar suggestions */
+user_pref("browser.urlbar.maxRichResults", 0);
+user_pref("browser.urlbar.suggest.bookmark", false);
+user_pref("browser.urlbar.suggest.history", false);
+user_pref("browser.urlbar.suggest.openpage", false);
+user_pref("browser.urlbar.suggest.searches", false);
+user_pref("browser.urlbar.suggest.topsites", false);
+
+/* block video autoplay */
+user_pref("media.autoplay.blocking_policy", 2);
+user_pref("media.autoplay.default", 5);
+
+/* block notification */
+user_pref("dom.webnotifications.enabled", false);
+user_pref("permissions.default.desktop-notification", 2);
+
+/* block gps */
+user_pref("permissions.default.geo", 2);
