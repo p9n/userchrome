a947f1cd4d0e815de2c3ac5afc9fc4584bc5accd
Aoyama Nanami <aoyama_nanami@sakurasou>
2021-06-02 00:53:30 +0800

    autoconfig: remove tab bar secondary text

--- install/autoconfig.js
+++ install/autoconfig.js
@@ -1,3 +1,5 @@
 pref("general.config.obscure_value", 0);
 pref("general.config.filename", "config.js");
 pref("general.config.sandbox_enabled", false);
+pref("browser.tabs.secondaryTextUnsupportedLocales",
+     "ar,bn,bo,ckb,fa,gu,he,hi,ja,km,kn,ko,lo,mr,my,ne,pa,si,ta,te,th,ur,zh,en");
